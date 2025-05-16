54667240b0798b46c76fc22f525ac1912cf91e05
Matei sHomePage <matei@mateishome.page>
2025-05-16 14:25:46 +0100

    css changes + username bubble (front-end only)

--- index.php
+++ index.php
@@ -8,12 +8,15 @@
         <header>
             <img src="/assets/images/logo.png" alt="mRSS Logo" class="fitheader">
             <input type="text" id="search" placeholder="Search your feeds..." class="search" autocomplete="off">
-            <div>
+            <div class="horizontal-flex-align-center">
                 <div class="vertical-flex-align-center">
                     <img src="/assets/images/cog.png" alt="Settings" height="50px" width="50px" >
                     Settings
                 </div>
-                <div>
-                    
-                </div>
+                <div class="user-profile-container">
+                    <img src="https://mateishome.page/applets/imgproxy.php?id=20" alt="User" class="user-profile-picture">
+                    <div>
+                        <h2>Username</h2>
+                        Logged in
+                    </div>
         </header>
